a77f3194c68d9d443dcf0f50f8958e4b0e64971e
Andy <andybk.nguyen1202@gmail.com>
2025-04-03 17:47:53 +0700

    add statistic feature

--- src/main/java/com/andy/iot/service/interf/StatisticServiceInterface.java
+++ src/main/java/com/andy/iot/service/interf/StatisticServiceInterface.java
@@ -1,2 +1,9 @@
-package com.andy.iot.service.interf;public interface StatisticServiceInterface {
+package com.andy.iot.service.interf;
+
+import com.andy.iot.dto.SensorRecordAvgDTO;
+
+import java.util.List;
+
+public interface StatisticServiceInterface {
+    List<SensorRecordAvgDTO> getWeeklyAverage(int weekType) throws Exception;
 }

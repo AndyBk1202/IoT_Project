a8b0ac468b204786e5afa37b777aaa381730f953
Andy <andybk.nguyen1202@gmail.com>
2025-04-14 22:22:24 +0700

    add schedule feature

--- src/main/java/com/andy/iot/service/DeviceControlService.java
+++ src/main/java/com/andy/iot/service/DeviceControlService.java
@@ -1,17 +1,26 @@
 package com.andy.iot.service;
 
+import com.andy.iot.model.Device;
+import com.andy.iot.model.DeviceStatusLog;
+import com.andy.iot.repository.DeviceRepository;
+import com.andy.iot.repository.DeviceStatusLogRepository;
 import com.andy.iot.response.ApiResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.List;
 import java.util.Map;
 
 @Service
+@RequiredArgsConstructor
 public class DeviceControlService {
     @Value("${adafruit.api-key}")
     private String apiKey;
+    private final DeviceRepository deviceRepository;
+    private final DeviceStatusLogRepository logRepo;
 
     private static final String BASE_URL = "https://io.adafruit.com/api/v2/Anhdo020204/feeds/";
 
@@ -46,5 +55,24 @@ public class DeviceControlService {
 
         // Trả về trạng thái mới
         return newValue;
+    }
+
+    public Integer getDeviceState(String device) throws Exception {
+        String url = BASE_URL + device + "/data/last";
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("X-AIO-Key", apiKey);
+        HttpEntity<String> entity = new HttpEntity<>(headers);
+
+        ResponseEntity<Map> response = restTemplate.exchange(url, HttpMethod.GET, entity, Map.class);
+        if (!response.getStatusCode().is2xxSuccessful() || response.getBody() == null) {
+            throw new Exception("Failed to retrieve current state from Adafruit IO.");
+        }
+
+        return Integer.parseInt(response.getBody().get("value").toString());
+    }
+
+    public List<Device> getAllDevice(){
+        return deviceRepository.findAll();
     }
 }
